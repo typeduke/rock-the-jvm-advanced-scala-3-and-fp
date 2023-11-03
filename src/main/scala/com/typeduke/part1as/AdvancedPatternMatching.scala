99cf046a5d813ca1f2589aae28b6b5fbd8f55491
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2023-11-03 16:28:17 +0000

    Add Add Taste of Advanced Scala - Advanced Pattern Matching - Part 2 of 2

--- src/main/scala/com/typeduke/part1as/AdvancedPatternMatching.scala
+++ src/main/scala/com/typeduke/part1as/AdvancedPatternMatching.scala
@@ -1,5 +1,7 @@
 package com.typeduke.part1as
 
+import java.{util => ju}
+
 object AdvancedPatternMatching {
   // We know we can use pattern matching with:
   // - constants
@@ -25,7 +27,7 @@ object AdvancedPatternMatching {
   }
 
   val daniel = new Person("Daniel", 102)
-  
+
   // This corresponds to checking that `Person.unapply(Daniel)` returns an `Option((n, a))`:
   val danielPatternMatched = daniel match {
     case Person(n, _) => s"Hi there, I'm $n!"
@@ -46,14 +48,14 @@ object AdvancedPatternMatching {
 
   val i: Int = 43
   val iPatternMatched = i match {
-    case even() => "An even number"
+    case even()        => "An even number"
     case singleDigit() => "A one-digit number"
-    case _ => "No special property"
+    case _             => "No special property"
   }
 
   // Infix patterns
   infix case class Or[A, B](a: A, b: B)
-  
+
   val anEither = Or(2, "two")
   val anEitherPatternMatched = anEither match {
     case n Or s => s"$n is written as \"$s\"."
@@ -62,12 +64,51 @@ object AdvancedPatternMatching {
   val aList = List(1, 2, 3)
   val aListPatternMatched = aList match {
     case 1 :: rest => "A list starting with 1"
-    case _ => "Some uninteresting list"
+    case _         => "Some uninteresting list"
   }
 
   // Decomposing sequences
-  // Continue here:
-  // A Taste of Advanced Scala - Advanced Pattern Matching - 16:00
+  val aListDecomposed = aList match {
+    case List(1, _*) => "A list starting with 1"
+    case _           => "Some other list"
+  }
+
+  abstract class MyList[A] {
+    def head: A = throw new NoSuchElementException
+    def tail: MyList[A] = throw new NoSuchElementException
+  }
+
+  case class Empty[A]() extends MyList[A]
+  case class Cons[A](override val head: A, override val tail: MyList[A]) extends MyList[A]
+
+  object MyList {
+    def unapplySeq[A](list: MyList[A]): Option[Seq[A]] =
+      if (list == Empty()) Some(Seq.empty)
+      else unapplySeq(list.tail).map(restOfSequence => list.head +: restOfSequence)
+  }
+
+  val myList: MyList[Int] = Cons(1, Cons(2, Cons(3, Empty())))
+  val myListDecomposed = myList match {
+    case MyList(1, _*) => "A list starting with 1"
+    case _             => "Some other list"
+  }
+
+  // Custom return type for `unapply` methods (to be used instead of `Option`)
+  abstract class Wrapper[T] {
+    def isEmpty: Boolean
+    def get: T
+  }
+
+  object PersonWrapper {
+    def unapply(person: Person): Wrapper[String] = new Wrapper[String] {
+      override def isEmpty = false
+      override def get = person.name
+    }
+  }
+
+  val weirdPersonPatternMatched = daniel match {
+    case PersonWrapper(name) => s"Hey, my name is $name."
+  }
 
   def main(args: Array[String]): Unit = {
     println(danielPatternMatched)
@@ -77,5 +118,10 @@ object AdvancedPatternMatching {
 
     println(anEitherPatternMatched)
     println(aListPatternMatched)
+
+    println(aListDecomposed)
+    println(myListDecomposed)
+
+    println(weirdPersonPatternMatched)
   }
 }
