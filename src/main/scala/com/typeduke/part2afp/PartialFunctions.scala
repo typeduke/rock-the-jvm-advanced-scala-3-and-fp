714ffd7a9ca6bffb2937eafef0900e7c371f65ba
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2023-11-06 17:32:26 +0000

    Add Advanced Functional Programming - Partial Functions - Part 2 of 2

--- src/main/scala/com/typeduke/part2afp/PartialFunctions.scala
+++ src/main/scala/com/typeduke/part2afp/PartialFunctions.scala
@@ -10,11 +10,12 @@ object PartialFunctions {
     else throw new RuntimeException("No suitable cases possible")
 
   // This function is only applicable to the `Int` instances 1, 2 and 5!
-  val aFussyFunction2 = (x: Int) => x match {
-    case 1 => 42
-    case 2 => 56
-    case 5 => 999
-  }
+  val aFussyFunction2 = (x: Int) =>
+    x match {
+      case 1 => 42
+      case 2 => 56
+      case 5 => 999
+    }
 
   // Partial function
   val aPartialFunction: PartialFunction[Int, Int] = { // As if `x => x match { ... }`
@@ -28,13 +29,44 @@ object PartialFunctions {
 
   val aLiftedPartialFunction: Int => Option[Int] = aPartialFunction.lift
 
-  val anotherPartialFunction: PartialFunction[Int, Int] = {
-    case 45 => 86
+  val anotherPartialFunction: PartialFunction[Int, Int] = { case 45 =>
+    86
   }
   val partialFunctionChain = aPartialFunction.orElse[Int, Int](anotherPartialFunction)
 
-  // Continue here:
-  // Advanced Functional Programming - Partial Functions - 09:00
+  // Higher-order functions accept partial functions...
+  // ...because `PartialFunction[A, B]` extends `Function1[A, B]`.
+  val aList = List(1, 2, 3, 4)
+
+  val aChangedList = aList.map({
+    case 1 => 4
+    case 2 => 3
+    case 3 => 45
+    case 4 => 67
+    case _ => 0
+  })
+
+  // Simplification thanks to the "single-parameter curly braces pattern":
+  val aChangedList2 = aList.map {
+    case 1 => 4
+    case 2 => 3
+    case 3 => 45
+    case 4 => 67
+    case _ => 0
+  }
+
+  case class Person(name: String, age: Int)
+
+  val someKids = List(
+    Person("Alice", 3),
+    Person("Bobbie", 5),
+    Person("Jane", 4)
+  )
+
+  // Partial functions are useful to deconstruct data structures right away and not in a lambda.
+  val someGrownUpKids = someKids.map { case Person(name, age) =>
+    Person(name, age + 1)
+  }
 
   def main(args: Array[String]): Unit = {
     println(aPartialFunction(2)) // 56
@@ -45,5 +77,8 @@ object PartialFunctions {
 
     // 45 is not supported by the first but the second partial function.
     println(partialFunctionChain(45)) // 86
+
+    println(someKids)
+    println(someGrownUpKids)
   }
 }
